bd648cdb3eaaa0e6d173f6d5502eee25e4b705bb
aauuxyxy <uuaaxyxy968@gmail.com>
2024-08-21 18:23:25 +0900

    🗃️ followテーブル定義変更

--- src/init/model/follow.go
+++ src/init/model/follow.go
@@ -1,7 +1,10 @@
 package model
 
 // フォロー
+// フォロワー数計算時はFollowIDに自身のIDが登録されている数＋Stateが2or3の数
 type Follow struct {
 	UsersID uint
 	FollowID uint
+	State int //0：両者フォロー解除、1：フォロー側フォロー、2：フォロワー側フォロー、3：相互フォロー
+	BlockTarget int // 0：ブロック無し、1：フォロワー側ブロック、2：フォロー側ブロック
 }
